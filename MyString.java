55dd61bb4f388e96e3ff036a1e29beed527504de
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-24 19:44:37 +0200

    update

--- MyString.java
+++ MyString.java
@@ -33,8 +33,6 @@ public class MyString {
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        str1 = lowerCase(str1);
-        str2 = lowerCase(str2);
         int[] s1 = new int[str1.length()];
         int[] s2 = new int[str2.length()];
         for (int i = 0 ; i < str1.length(); i ++){
--- Primes.java
+++ Primes.java
@@ -1,6 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        int count = 0;
         System.out.println("Prime numbers up to " + n);
         for (int i = 2 ; i <= n ; i++){
             int j = 2;
@@ -10,8 +11,11 @@ public class Primes {
                 }
             }
             if (j == i){
+                count++;
                 System.out.println(i);
             }
         }
+        int percent = 100 * count / n;
+        System.out.println("There are " + count + " primes between 2 and " + n +  " (" + percent + "% are primes)");
     }
 }
