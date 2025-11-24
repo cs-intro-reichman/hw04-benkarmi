5cc27c2e6501414e3a043619c3c974fb63292361
benkarmi <69310049+benkarmi@users.noreply.github.com>
2025-11-24 19:36:55 +0200

    update

--- Primes.java
+++ Primes.java
@@ -1,5 +1,17 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to " + n);
+        for (int i = 2 ; i <= n ; i++){
+            int j = 2;
+            for (j = 2 ; j < i ; j++){
+                if (i%j == 0){
+                    break;
+                }
+            }
+            if (j == i){
+                System.out.println(i);
+            }
+        }
     }
 }
